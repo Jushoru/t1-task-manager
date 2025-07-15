4e8865ba69adc477e1c8b730de0af6388f3e90f1
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-07-16 01:35:02 +0300

    build: add FSD structure and minor chore changes

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import {Button} from "@mui/material";
+
+function App() {
+
+  return (
+    <>
+      <h1>Менеджер задач</h1>
+        <Button variant="contained">кнопчег</Button>
+    </>
+  )
+}
+
+export default App
